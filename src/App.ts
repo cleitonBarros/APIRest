26542c0193eca7bacdf455b956aa0594194646f3
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-04-22 18:40:21 -0300

    created and config  morgan's middleware

--- src/App.ts
+++ src/App.ts
@@ -4,10 +4,14 @@ import config  from "config";
 import router from "./router";
 import  db from "../config/db";
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
 
 const app = express();
 //json middleware
 app.use(express.json())
+
+app.use(morganMiddleware)
+
 app.use("/api/", router)
 
 
