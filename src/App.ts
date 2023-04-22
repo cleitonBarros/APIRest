526a1e0872aa8cd850901b4b053d598c969d3e25
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-04-22 17:12:31 -0300

    created router and port

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,15 @@
-console.log('deu certo')
+import express  from "express";
+import config  from "config";
+import router from "./router";
+
+const app = express();
+//json middleware
+app.use(express.json())
+
+app.use("/api/", router)
+
+//porta
+const port = config.get<number>('port');
+app.listen(port, async ()=>{
+    console.log(`http://localhost:${port}`)
+});
